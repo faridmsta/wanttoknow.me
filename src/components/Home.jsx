e95719084cf1584db0fdbd9349b9302bd0356349
faridmsta <faridmustafyev@gmail.com>
2024-02-18 14:12:40 +0400

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,14 @@ import 'aos/dist/aos.css'
 import '../components/Home.css'
 import Meg from '../components/img/megradient.png'
 import Click from '../components/sound/click.mp3'
-
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
 function Home() {
-
+  const allicons = document.querySelectorAll('.round .icon')
   useEffect(() => {
     Aos.init();
   }, [])
@@ -21,6 +26,16 @@ function Home() {
     document.querySelector('.mainContent').style.opacity = "1"
   }, 500)
 
+  function showlink(e) {
+    for (let i = 0; i < allicons.length; i++) {
+      if (allicons[i].classList != e.currentTarget.classList) {
+        allicons[i].classList.remove('openlink')
+      }
+
+    }
+    e.currentTarget.classList.toggle('openlink')
+
+  }
 
   return (
 
@@ -57,23 +72,62 @@ function Home() {
         </div>
       </section>
       <section className="social">
+        <div className="container">
+          <div className="socialWrap">
+            <div className="head">
+              <h2>Let's Connect</h2>
+              <p>Just tap on the icons and and follow the link</p>
+            </div>
+            <div className="socialM">
+              <div className="round">
+                <div onClick={showlink} className='icon'>
+                  <FaFacebookSquare />
+                  <div className="ltoR">
+                    <a href="https://www.facebook.com/" target="_blank">Farid_Mustafayev</a>
+                    <FaArrowRight />
+                  </div>
+                </div>
+                <div onClick={showlink} className='icon'>
+                  <FaInstagram />
+                  <div className="ltoR">
+                    <a href="https://www.instagram.com/faridmsta/" target="_blank">@faridmsta</a>
+                    <FaArrowRight />
+                  </div>
+                </div>
+                <div onClick={showlink} className='icon'>
+                  <FaGithub />
+                  <div className="ltoR">
+                    <a href="https://github.com/faridmsta" target="_blank">faridmsta</a>
+                    <FaArrowRight />
+                  </div>
+                </div>
+                <div onClick={showlink} className='icon'>
+                  <FaLinkedin />
+                  <div className="ltoR">
+                    <a href="https://www.linkedin.com/in/faridmustafayev/" target="_blank">Farid_Mustafayev</a>
+                    <FaArrowRight />
+                  </div>
+                </div>
+                <div onClick={showlink} className='icon'>
+                  <FaYoutube />
+                  <div className="ltoR">
+                    <a href="https://www.youtube.com/channel/UCxpfu2JXCY8ZQVijUnyGllA" target="_blank">Farid_Mustafayev</a>
+                    <FaArrowRight />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="d3dSocialD">
+              <Spline
+                style={{
+                  height: "60vh",
+                  width: screen.width < 1024 ? '100vw' : '700px'
+                }}
+                scene="https://prod.spline.design/U5sCJH5Q8vbAcyi5/scene.splinecode"
+              />
+            </div>
 
-        <div className="socialWrap">
-          <div className="d3dSocialM">
-            <Spline
-              style={{
-                width: ' 100%',
-                height: '100vh'
-              }}
-              scene="https://prod.spline.design/4SpYPPL10nVCERQ8/scene.splinecode"
-            />
-
-            {/* <Spline
-              scene="https://prod.spline.design/U5sCJH5Q8vbAcyi5/scene.splinecode"
-
-            /> */}
           </div>
-
         </div>
       </section>
     </main>
